018c129a39273d7eee023aa4cba737eddc593c88
DashaErmolich <dasha.ermolich@gmail.com>
2023-01-07 20:22:17 +0100

    feat: add test run to github actions config

---
+++ src/__tests__/test2.unit.ts
@@ -0,0 +1,23 @@
+import type * as TestFunctions from '../sum'
+
+const { sum } = jest.requireActual<typeof TestFunctions>('../sum.ts');
+
+const successCases = [
+  {
+    id: 0,
+    input: {a: 1, b: 1},
+    output: 2,
+  },
+  {
+    id: 1,
+    input: {a: 2, b: 3},
+    output: 5,
+  },
+]
+
+describe('Test sum function2', () => {
+  it.each(successCases)('success case $id', ({ input, output }) => {
+    const { a, b } = input;
+    expect(sum(a, b)).toBe(output);
+  })
+})
